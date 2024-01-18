f08ce9c39b20923b0c5759ab5fbbb06c177d7d42
Carmelo Bongiorno <c.bongiorno@reply.it>
2024-01-18 11:56:43 +0100

    Added 2 new  variables

---
+++ module/subscriptions/variables.tf
@@ -0,0 +1,14 @@
+variable "subscriptions" {
+  type = map(object({
+    subscription_name = string
+    billing_scope_id  = string
+  }))
+}
+
+variable "billing_enrollment_account_scope" {
+    type = object({
+      billing_account_name = string
+      enrollment_account_name = string
+    })
+  
+}
